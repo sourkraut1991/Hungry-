c0018465e20147d3fa4f96d8eddc867d6ad121ee
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2023-03-18 03:30:48 +0300

    Initial Commit

---
+++ Hungry?/Hungry_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hungry_App.swift
+//  Hungry?
+//
+//  Created by Ed Kraus on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Hungry_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
